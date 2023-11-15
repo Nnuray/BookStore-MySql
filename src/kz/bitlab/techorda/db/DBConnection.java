bf2627597e06dcbfe9205cf08ef91221ec84f64a
Nuray <nuray.kavkaeva.04@gmail.com>
2023-11-15 19:39:28 +0600

    second commit

--- src/kz/bitlab/techorda/db/DBConnection.java
+++ src/kz/bitlab/techorda/db/DBConnection.java
@@ -1,14 +1,46 @@
 package kz.bitlab.techorda.db;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
 
 public class DBConnection {
-    private Connection connection;
+    private static Connection connection;
     static {
         try{
-
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection = DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306/tech_orda_db",
+                    "root",
+                    "31a07a2004");
         } catch (Exception e){
             e.printStackTrace();
         }
+    }
+
+    public static ArrayList<Book> getBooks(){
+        ArrayList<Book> books = new ArrayList<>();
+        try{
+            PreparedStatement statement = connection.prepareStatement(
+                    "SELECT * FROM books");
+            ResultSet resultSet = statement.executeQuery();
+            while (resultSet.next()){
+                Book book = new Book();
+                book.setId(resultSet.getInt("id"));
+                book.setName(resultSet.getString("name"));
+                book.setAuthor(resultSet.getString("author"));
+                book.setGenre(resultSet.getString("genre"));
+                book.setPrice(resultSet.getInt("price"));
+                book.setDescription(resultSet.getString("description"));
+                books.add(book);
+            }
+
+            statement.close();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return books;
     }
 }
